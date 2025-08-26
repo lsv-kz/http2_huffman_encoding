201974cee0144d83a31f94d11c26a248f936e659
lsv-kz <010462kz@gmail.com>
2025-08-26 19:44:27 +0500

    initial project version

---
+++ bytes_array.h
@@ -0,0 +1,181 @@
+#ifndef BYTESARRAY_H_
+#define BYTESARRAY_H_
+
+#include <iostream>
+#include <cstring>
+
+//======================================================================
+class ByteArray
+{
+    char *buf;
+    int buf_size;
+    int buf_len;
+    int offset;
+    //------------------------------------------------------------------
+    int resize(int size_new)
+    {
+        if ((buf_size >= size_new) ||(size_new <= 0))
+            return -1;
+        char *tmp_buf = new(std::nothrow) char [size_new];
+        if (!tmp_buf)
+        {
+            fprintf(stderr, "<%s:%d> Error new char [%d]\n", __func__, __LINE__, size_new);
+            return -1;
+        }
+
+        if (buf)
+        {
+            if (buf_len > 0)
+                memcpy(tmp_buf, buf, buf_len + 1);
+            delete [] buf;
+        }
+
+        buf = tmp_buf;
+        buf_size = size_new;
+
+        return 0;
+    }
+
+    ByteArray(const ByteArray&);
+    ByteArray& operator=(const ByteArray&);
+
+public:
+
+    ByteArray()
+    {
+        buf = NULL;
+        buf_size = buf_len = offset = 0;
+    }
+    //------------------------------------------------------------------
+    ~ByteArray()
+    {
+        if (buf)
+        {
+            delete [] buf;
+            buf = NULL;
+        }
+    }
+    //------------------------------------------------------------------
+    void init()
+    {
+        buf_len = offset = 0;
+    }
+    //------------------------------------------------------------------
+    void reserve(unsigned int n)
+    {
+        resize(n);
+    }
+    //------------------------------------------------------------------
+    int cpy(const char *b, int len)
+    {
+        if (len <= 0)
+        {
+            fprintf(stderr, "<%s:%d> len=%d\n", __func__, __LINE__, len);
+            return -1;
+        }
+
+        if (buf_size <= len)
+        {
+            if (resize(len + 64))
+                return -1;
+        }
+
+        memcpy(buf, b, len);
+        buf_len = len;
+        offset = 0;
+        buf[buf_len] = 0;
+        return 0;
+    }
+    //------------------------------------------------------------------
+    int cat(const char *b, int len)
+    {
+        if (len <= 0)
+        {
+            fprintf(stderr, "<%s:%d> len=%d\n", __func__, __LINE__, len);
+            return -1;
+        }
+
+        if (buf_size <= (buf_len + len))
+        {
+            if (resize(buf_len + len + 64))
+                    return -1;
+        }
+
+        memcpy(buf + buf_len, b, len);
+        buf_len += len;
+        buf[buf_len] = 0;
+        return 0;
+    }
+    //------------------------------------------------------------------
+    int cat(const char b)
+    {
+        if (buf_size <= (buf_len + 1))
+        {
+            if (resize(buf_len + 64))
+                    return -1;
+        }
+
+        buf[buf_len] = b;
+        buf_len += 1;
+        buf[buf_len] = 0;
+        return 0;
+    }
+    //------------------------------------------------------------------
+    int cpy_str(const char *s)
+    {
+        int len = (int)strlen(s);
+        if (len)
+            return cpy(s, len);
+        else
+        {
+            buf_len = 0;
+            offset = 0;
+            return 0;
+        }
+    }
+    //------------------------------------------------------------------
+    int cat_str(const char *s)
+    {
+        int len = (int)strlen(s);
+        if (len)
+            return cat(s, len);
+        else
+        {
+            return 0;
+        }
+    }
+    //------------------------------------------------------------------
+    const char *ptr()
+    {
+        if (buf)
+        {
+            buf[buf_len] = 0;
+            return buf;
+        }
+        else
+            return "";
+    }
+    //------------------------------------------------------------------
+    int get_byte(int i)
+    {
+        if ((i >= buf_len) || (i < 0))
+            return -1;
+        return (unsigned char)buf[i];
+    }
+    //------------------------------------------------------------------
+    int set_byte(char ch, int i)
+    {
+        if ((i < 0) || (i >= buf_len) || (buf_size == 0))
+            return -1;
+        else
+        {
+            buf[i] = ch;
+            return 0;
+        }
+    }
+    //------------------------------------------------------------------
+    int size() { return buf_len; }
+    //------------------------------------------------------------------
+};
+
+#endif
---
+++ class/test/test.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <cstring>
+#include <sys/time.h>
+
+#include "../huffman.h"
+
+using namespace std;
+//======================================================================
+void std_in(char *s, int len)
+{
+    char ch;
+    
+    while ((ch = getchar()) != '\n' && (len - 1))
+    {
+        *s++ = ch;
+        len--;
+    }
+    *s = '\0';
+    if (ch != '\n')
+        while ((ch = getchar()) != '\n');
+}
+//======================================================================
+void get_time(struct timeval *time1, const char *s)
+{
+    unsigned long ts12, tu12;
+    struct timeval time2;
+
+    gettimeofday(&time2, NULL);
+
+    if ((time2.tv_usec-time1->tv_usec) < 0)
+    {
+        tu12 = (1000000 + time2.tv_usec) - time1->tv_usec;
+        ts12 = (time2.tv_sec - time1->tv_sec) - 1;
+    }
+    else
+    {
+        tu12 = time2.tv_usec - time1->tv_usec;
+        ts12 = time2.tv_sec - time1->tv_sec;
+    }
+
+    fprintf(stderr, "%s: %lu.%06lu sec\n", s, ts12, tu12);
+}
+//======================================================================
+void hex_print_stderr(const char *s, const void *p, int n)
+{
+    int count, addr = 0, col;
+    unsigned char *buf = (unsigned char*)p;
+    char str[18];
+    fprintf(stderr, "---------------- %s ----------------\n", s);
+    for(count = 0; count < n;)
+    {
+        fprintf(stderr, "%08X  ", addr);
+        for(col = 0, addr = addr + 0x10; (count < n) && (col < 16); count++, col++)
+        {
+            if (col == 8) fprintf(stderr, " ");
+            fprintf(stderr, "%02X ", *(buf+count));
+            str[col] = (*(buf + count) >= 32 && *(buf + count) < 127) ? *(buf + count) : '.';
+        }
+        str[col] = 0;
+        if (col <= 8) fprintf(stderr, " ");
+        fprintf(stderr, "%*s  %s\n",(16 - (col)) * 3, "", str);
+    }
+
+    fprintf(stderr, "\n");
+}
+//======================================================================
+int main(int count, char *strings[])
+{
+    HuffmanCode huff;
+    ByteArray buf;
+    struct timeval time1;
+    char s[4096];
+    string str;
+    str.reserve(128);
+
+    while (true)
+    {
+        printf("============================================\n"
+               ">>> ");
+        fflush(stdout);
+        std_in(s, sizeof(s));
+        //--------------------------------------------------------------
+        huff.encode(s, buf);
+        hex_print_stderr("encode", buf.ptr(), buf.size());
+
+gettimeofday(&time1, NULL);
+        huff.decode(buf.ptr(), buf.size(), str);
+get_time(&time1, "decode time");
+
+        fprintf(stdout, "decode:\n [%s]\n", s);
+        fprintf(stdout, " [%s]\n", str.c_str());
+    }
+
+    return 0;
+}
+
+//  Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0
+//  現代標準漢語   (Chinese)
+//  У Вас продается славянский шкаф?   (Russian)
